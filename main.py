825541a9c277ab03f98753c526d46c08f2ed57b3
jakehulberg <149437250+jakehulberg@users.noreply.github.com>
2023-10-30 17:45:16 -0500

    Update main.py
    Combined some categories because I'll just do this for the chase cards for now. Next I want to define the functions as a monthly and annual inputs and give the user the option.

--- main.py
+++ main.py
@@ -6,16 +6,43 @@ Total_Spend = input("Total Spend (per month)= ")
 Travel = int(input("Monthly Travel Spend = "))
 Dining = int(input("Monthly Dining Spend = "))
 Gas = int(input("Monthly Gas Spend = "))
+Bills_Utilities = int(input("Monthly Bills and Utilities Spend = "))
+Education = int(input("Monthly Education Spend = "))
+Entertainment = int(input("Monthly Entertainment Spend = "))
+Personal = int(input("Monthly Personal Spend = "))
+Home = int(input("Monthly Home Spend = "))
 Groceries = int(input("Monthly Groceries Spend = "))
+The_rest = int(Gas + Bills_Utilities + Education + Entertainment + Personal + Home)
+
 
 print("\nOkay. So you spend " + str(Total_Spend) + " dollars per month total. " + str(Travel) + " on travel, ")
 print(str(Dining) + " on dining, " + str(Gas) + " on gas, and " + str(Groceries) + " on groceries. ")
 
-csp_rewards = int((Travel * .03) + (Dining * .03) + (Gas * .01) + (Groceries * .01))
-csr_rewards = int(((Travel + Dining) * .04) + ((Gas + Groceries) * .10))
 
-if csp_rewards > csr_rewards:
-    print("\nWhereas with the Chase Sapphire reserve, your monthly rewards would be " + str(csr_rewards))
+# important variables for CSP here
+csp_travel_credit = 50
+csp_annual_fee = 95
+csp_rewards_multiplier = 1.25
+csp_annual_adjusted_fee = 45
+csp_monthly_rewards = (csp_rewards_multiplier * int((Travel * .02) + (Dining * .03) + (The_rest * .01) - (csp_annual_adjusted_fee / 12)))
+
+# important variables for CSR here
+csr_travel_credit = 300
+csr_annual_fee = 550
+csr_rewards_multiplier = 1.5
+csr_annual_adjusted_fee = 250
+csr_additional_user_fee = 75
+csr_monthly_rewards = (csr_rewards_multiplier * int((Travel + Dining) * .03 + (The_rest * .01))) - (csr_annual_adjusted_fee / 12)
+
+
+
+# printing my results
+
+if csp_monthly_rewards > csr_monthly_rewards:
+    print("\nYou should go with the Chas Sapphire Preferred. Your monthly rewards will be " + str(csp_monthly_rewards))
+    print("\nWhereas with the Chase Sapphire reserve, your monthly rewards would be " + str(csr_monthly_rewards))
+    print("\nYour annual rewards would be " + str(csp_monthly_rewards * 12))
 else:
-   print("\nYou should go with the Chase Sapphire Reserve, as your monthly rewards will be $" + str(csr_rewards))
-   print("\nWhereas with the Chase Sapphire preferred, your monthly rewards would be $" + str(csp_rewards))
+   print("\nYou should go with the Chase Sapphire Reserve, as your monthly rewards will be $" + str(csr_monthly_rewards))
+   print("\nWhereas with the Chase Sapphire preferred, your monthly rewards would be $" + str(csp_monthly_rewards))
+   print("\nYour annual rewards would be $" + str(csr_monthly_rewards * 12))
